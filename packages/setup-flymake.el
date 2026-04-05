5d2c9e2d94fd9c606c7201b6cc9bdf881673f4fb
Nikita Pavshintsev <germanoak08@gmail.com>
2026-04-05 13:57:39 +0300

    stop flymake from highlighting elisp config files

--- packages/setup-flymake.el
+++ packages/setup-flymake.el
@@ -1,7 +1,7 @@
 (use-package flymake
   :ensure nil
   :defer t
-  :hook (prog-mode-hook . flymake-mode)
+  :hook ((prog-mode . my/maybe-enable-flymake))
   :bind (:map flymake-mode-map
               ("M-9" . flymake-goto-next-error)
               ("M-8" . flymake-goto-prev-error)
@@ -10,25 +10,34 @@
               ("C-c ! l" . flymake-show-buffer-diagnostics)
               ("C-c ! t" . toggle-flymake-diagnostics-at-eol))
   :custom
-  ;; (flymake-show-diagnostics-at-end-of-line nil)
   (flymake-show-diagnostics-at-end-of-line 'short)
   (flymake-indicator-type 'margins)
   (flymake-margin-indicators-string
-   `((error "!" compilation-error)      ;; Alternatives: », E, W, i, !, ?)
+   `((error "!" compilation-error)
      (warning "?" compilation-warning)
      (note "i" compilation-info)))
   :config
-  ;; Define the toggle function
+  (defun my/maybe-enable-flymake ()
+    (unless (my/elisp-config-file-p)
+      (flymake-mode 1)))
+
+  (defun my/elisp-config-file-p ()
+    (and buffer-file-name
+         (derived-mode-p 'emacs-lisp-mode)
+         (file-in-directory-p
+          (file-truename buffer-file-name)
+          (file-truename user-emacs-directory))))
+
   (defun toggle-flymake-diagnostics-at-eol ()
     "Toggle the display of Flymake diagnostics at the end of the line
 and restart Flymake to apply the changes."
     (interactive)
     (setq flymake-show-diagnostics-at-end-of-line
           (not flymake-show-diagnostics-at-end-of-line))
-    (flymake-mode -1) ;; Disable Flymake
-    (flymake-mode 1)  ;; Re-enable Flymake
+    (flymake-mode -1)
+    (flymake-mode 1)
     (message "Flymake diagnostics at end of line: %s"
              (if flymake-show-diagnostics-at-end-of-line
                  "Enabled" "Disabled"))))
 
-(provide 'setup-flycheck)
+(provide 'setup-flymake)
