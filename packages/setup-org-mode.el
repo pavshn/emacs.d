35b3bf0562cdb8f804e19ca16ece673027b846e6
Nikita Pavshintsev <germanoak08@gmail.com>
2026-05-21 15:23:26 +0300

    add setup-org-mode.el

---
+++ packages/setup-org-mode.el
@@ -0,0 +1,29 @@
+(use-package org
+  :ensure nil
+
+  :init
+  (setq org-directory "~/org"
+        org-default-notes-file
+        (expand-file-name "inbox.org" org-directory))
+
+  :custom
+  ;; Visual
+  (org-startup-indented t)
+  (org-hide-emphasis-markers t)
+  (org-startup-folded 'content)
+
+  ;; UX
+  (org-return-follows-link t)
+
+  ;; TODO logging
+  (org-log-done 'time)
+
+  ;; Quick capture
+  (org-capture-templates
+   '(("i" "Inbox"
+      entry
+      (file "~/org/inbox.org")
+      "* %?\n%U\n")))
+
+  :bind
+  ("C-c c" . org-capture))
